6fa9f967f9aee3f0a4eefdc918d77f55ad9d0314
Hayk <jomardyan@outlook.com>
2021-07-15 17:08:20 +0200

    update

--- CRIF-Encrypt/Program.cs
+++ CRIF-Encrypt/Program.cs
@@ -7,24 +7,22 @@ namespace CRIF_Encrypt
 {
     internal class Program
     {
-        private static string EncryptCommand(string Xpath)
-        {
-            StringBuilder st = new StringBuilder();
-            string command;
-            string b = "\"";
-            command = "/C gpg.exe -v -se  -r CRIF-SWO-PROD  --passphrase \"\"";
-            st.Append(command);
-            st.Append(" ");
-            st.Append(b + Xpath + b);
-            return st.ToString();
-        }
+
 
         private static void Main(string[] args)
         {
             if (args.Length == 0)
+                Console.WriteLine(""); 
                 return; // return if no file was dragged onto exe
             string text = File.ReadAllText(args[0]);
             text = text.Replace("~", "~\r\n");
+
+            //FOR ZIP PURPOSE
+            string dir = Path.GetDirectoryName(args[0])
+               + Path.DirectorySeparatorChar;
+            string filename = Path.GetFileNameWithoutExtension(args[0]);
+            //FOR ZIP PURPOSE END 
+
             string path = Path.GetDirectoryName(args[0])
                + Path.DirectorySeparatorChar
                + Path.GetFileNameWithoutExtension(args[0])
@@ -32,6 +30,7 @@ namespace CRIF_Encrypt
             File.WriteAllText(path, text);
             Console.WriteLine("     (c) Hayk Jomardyan 2021. All rights reserved.\n");
             Console.WriteLine("     ... \n " + "Selected file: " + path + "\n");
+            Console.WriteLine("     ... \n " + "Selected dir: " + dir + "\n");
             Console.WriteLine("Command: " + EncryptCommand(path) + "\n");
 
             //simplified:: System.Diagnostics.Process.Start("CMD.exe", EncryptCommand(path));
@@ -52,9 +51,52 @@ namespace CRIF_Encrypt
 
                 Console.WriteLine("Error: {0}", e.Message);
             }
-            
+
+
 
             var x = Console.ReadLine();
         }
+
+
+        static string EncryptCommand(string Xpath)
+        {
+            StringBuilder st = new StringBuilder();
+            string command;
+            string b = "\"";
+            command = "/C gpg.exe -v -se  -r CRIF-SWO-PROD  --passphrase \"\"";
+            st.Append(command);
+            st.Append(" ");
+            st.Append(b + Xpath + b);
+            return st.ToString();
+        }
+
+        static bool zipping(string OutputPath, string ImputFile)
+        {
+
+            StringBuilder cm = new StringBuilder();
+            cm.Append("a "); 
+            cm.Append(OutputPath + " ");
+            cm.Append(ImputFile); 
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "C:\\Program Files\\7-Zip\\7z.exe";
+                startInfo.Arguments = "";
+                process.StartInfo = startInfo;
+                process.Start();
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Error: {0}", e.Message);
+                return false;
+            }
+
+
+        }
+
+
     }
 }
